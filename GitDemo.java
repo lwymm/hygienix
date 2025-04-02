68b3fde1efa12a79495fd7cac4322e795439d9c2
Patrick <riverapatrickjohn042501@gmail.com>
2025-04-02 15:43:17 +0800

    Germ Kill

---
+++ GitDemo.java
@@ -0,0 +1,7 @@
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git Branching Strategy Demo!");
+        System.out.println("Your Name: Patrick John Rivera   ”);
+        System.out.println(“Your” Capability: DWP);
+    }
+}
